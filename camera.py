cb2a3e86064d594cc8c281ab26503af58752145a
ZaneDaPayne <Zane.Acosta@Gmail.com>
2020-05-25 23:09:08 -0700

    Currently equivalent to Working

--- camera.py
+++ camera.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Mon May  4 00:32:17 2020
 
-Might get camera to work on android using AndroidCamera class.
 
 @author: Zane
 """
@@ -10,19 +8,20 @@ Might get camera to work on android using AndroidCamera class.
 
 from kivymd.app import MDApp
 from kivy.uix.screenmanager import Screen, ScreenManager
-from kivy.uix.image import Image
+#from kivy.uix.image import Image
 import os
 from kivy.garden.matplotlib.backend_kivyagg import FigureCanvasKivyAgg
 import matplotlib.pyplot as plt
 from kivy.properties import DictProperty
 from kivy.utils import platform
+from kivy.garden.xcamera import XCamera
 
 class Screenmanager(ScreenManager):
     pass
 
 class StartScreen(Screen):
     pass
-
+    
 class MainWindow(Screen):
     def __init__(self,**kwargs):
         super().__init__(**kwargs)
@@ -39,14 +38,7 @@ class MainWindow(Screen):
         from android.permissions import request_permission, Permission
         request_permission(Permission.CAMERA)
         
-    def capture(self):
-        camera=self.ids.camera
-        camera.export_to_png("IMG_domino.png")
-            
-    def detect_domino(self,im):
-        im = plt.imread("IMG_domino.png")
-        #some processing with im
-            
+        
     def cyclecam(self):
         index = self.ids.camera.index
         try:
@@ -55,7 +47,7 @@ class MainWindow(Screen):
         except:
             self.ids.camera.index = 0
 
-class OverlayWindow(Screen):  
+class OverlayWindow(Screen): 
     def __init__(self, **kwargs):
         self.add_blanks_val = 0
         self.add_dots_val = 0
@@ -99,46 +91,39 @@ class OverlayWindow(Screen):
         self.update_score()
     
     def update_score(self):
-        self.score = self.num_dots + self.num_blanks*25
+        self.score = (self.num_dots+self.add_dots_val)+(self.num_blanks+self.add_blanks_val)*25
         self.ids.score_label.text = f"Score: {self.score}"
-      
-    def removeimg(self):
-        try:
-            os.remove("IMG_domino.png")
-        except:
-            pass
+        print(self.ids.score_label.text)
         
+    def reset(self):
+        try:
+            os.remove("IMG_domino.jpg")
+        except:
+            pass    
         try:
             app = MDApp.get_running_app()
             self.ids.checkwindow.remove_widget(app.dynamic_ids.plot) # remove the plot
         except:
             pass
-        self.num_dots, self.num_blanks = 0,0
+        self.num_dots, self.num_blanks, self.add_dots_val, self.add_blanks_val = 0,0,0,0
         self.update_score()
-        
-    def createimg(self):
-        im = plt.imread("IMG_domino.png")
-        fig,ax = plt.subplots()
-        ax.imshow(im)
-        ax.axis("off")
-        self.drawimg()
-        
-    def drawimg(self):
-        app = MDApp.get_running_app()
-        plot = FigureCanvasKivyAgg(plt.gcf(),pos_hint={"top":1})
-        self.ids.checkwindow.add_widget(plot) # add plot to the check window
-        
-        
-        app.dynamic_ids['plot'] = plot
-        plt.close() #closes figures for the next run
-    
+        self.ids.blank_count.text = f"BLANKS +0"
+        self.ids.dot_count.text = f"DOTS +0"
 
 class CameraApp(MDApp):
     dynamic_ids = DictProperty({}) # my own dictionary for dynamic widget ids
+    
+    
     def build(self):
         pass #the kv file does the building
     
        
 if __name__=="__main__":
     CameraApp().run()
+
+
+
+
+
+
 
