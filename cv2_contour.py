dc0ba3b81f80f67a24e735723208c9bbe25fa386
ZaneDaPayne <Zane.Acosta@Gmail.com>
2020-06-01 00:29:52 -0700

    Runs convolution first. Need to get out put of convolution in a cv2 friendly format

--- cv2_contour.py
+++ cv2_contour.py
@@ -2,13 +2,58 @@
 """
 Created on Sun May 31 23:53:06 2020
 
+I think convolution then contour deteciton would be good.
+Can you help me with this? I don't know how to have the output_image
+be saved a bgr image so cv2 can detect the contours.
+The less reading and writing to storage (saving and opening images)
+the better.
+
 @author: Zane
 """
+
+
+#Convolution
+from PIL import ImageDraw
+import PIL
+import matplotlib.pyplot as plt
+import numpy as np
+from math import sqrt
+
+# Load image:
+input_image = PIL.Image.open("Dominoes.jpg")
+input_pixels = input_image.load()
+width, height = input_image.width, input_image.height
+
+# Create output image
+output_image = PIL.Image.new("RGB", input_image.size)
+draw = ImageDraw.Draw(output_image)
+
+# Convert to grayscale
+intensity = np.zeros((width, height))
+for x in range(width):
+    for y in range(height):
+        intensity[x, y] = sum(input_pixels[x, y]) / 3
+index=0
+# Compute convolution between intensity and kernels
+for x in range(1, input_image.width - 1):
+    for y in range(1, input_image.height - 1):
+        magx = intensity[x + 1, y] - intensity[x - 1, y]
+        magy = intensity[x, y + 1] - intensity[x, y - 1]
+
+        # Draw in black and white the magnitude
+        color = int(sqrt(magx**2 + magy**2))
+        draw.point((x, y), (color, color, color))
+    index = index + 1
+         
+output_image.show()
+print(output_image.shape)
+
+
 import matplotlib.pyplot as plt
 from matplotlib.patches import Rectangle,Circle
 import cv2
 
-im = cv2.imread("Dominoes.jpg")        
+im = cv2.imread("edge.jpg")# this is bgr. I want this to be output_image     
 b,g,r = cv2.split(im)#convert to rgb
 im_c = cv2.merge([r,g,b])
 
@@ -38,3 +83,5 @@ for dot in dots:
     circ = Circle(xy,radius,fc='none',ec='yellow',lw=2)
     ax.add_patch(circ,)
 plt.show()
+
+
